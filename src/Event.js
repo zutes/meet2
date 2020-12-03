e40046c59c9471f8bf8bcffa493c2e9523190257
zutes <zutes3479@gmail.com>
2020-12-03 09:11:17 -0700

    Updated files

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,50 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    showDetails: false,
+  };
+
+  handleShowDetails = () => {
+    if (this.state.showDetails === false) {
+      this.setState({ showDetails: true });
+    } else {
+      this.setState({ showDetails: false });
+    }
+  };
+
   render() {
-    return <div></div>;
+    const showDetails = this.state.showDetails;
+    const { event } = this.props;
+
+    return (
+      <div className="event">
+        <div className="event-overview">
+          <h2 className="summary">{event.summary}</h2>
+          <p className="location">{event.location}</p>
+          <p className="start-time">{event.start.dateTime}</p>
+          {!showDetails && (
+            <button className="details-btn" onClick={this.handleShowDetails}>
+              Show Event Details
+            </button>
+          )}
+          {showDetails && (
+            <button className="details-btn" onClick={this.handleShowDetails}>
+              Hide Event Details
+            </button>
+          )}
+        </div>
+
+        {showDetails && (
+          <div className="event-details">
+            <p className="description">{event.description}</p>
+            <p className="end-time">{event.end.dateTime}</p>
+            <p className="time-zone">{event.start.timeZone}</p>
+            <p className="organizer-email">{event.organizer.email}</p>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 export default Event;
