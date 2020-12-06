ef6f155bed526952f6494f0407aefeae4965d990
zutes <zutes3479@gmail.com>
2020-12-06 10:05:23 -0700

    Update endpoint

--- src/api.js
+++ src/api.js
@@ -30,7 +30,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = `YOUR_GET_EVENTS_API_ENDPOINT/${token}`;
+    const url = `https://7fjff7sh6e.execute-api.us-east-2.amazonaws.com/dev/api/get-events/${token}`;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
