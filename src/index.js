343dd6a1480e6d12e7f8849a2b5c518d50206ee0
zutes <zutes3479@gmail.com>
2020-12-11 12:33:52 -0700

    Updated file to integrate Atatus for testing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +17,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//serviceWorker.unregister();
+atatus.config('921252d3764d411a9b1aeb81d92f7845').install();
+
