7b3d2554cf610d081c7f496c2f43a822583c0877
zutes <zutes3479@gmail.com>
2020-12-15 12:44:19 -0700

    Updated NumberofEvent.js file
    Added code to incorporate the ErrorAlert for number of displayed events.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,30 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
-
   state = {
     numberOfEvents: 32,
-  }
+  };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.props.updateEvents(null, value);
     this.setState({
-       numberOfEvents: value
-     });
-  }
+      numberOfEvents: value,
+    });
+
+    if (value < 1 || value >32) {
+      this.setState({
+        infoText: "Choose a number of events between 1 and 32",
+      });
+    } else {
+      this.setState({
+        infoText: "",
+      });
+    }
+  };
 
   render() {
     return (
-      <div className='number-of-events'>
-        <label className='number-of-events-label'>Number of Events: </label>
+      <div className="number-of-events">
+        <ErrorAlert text={this.state.infoText} />
+        <label className="number-of-events-label">Number of Events: </label>
         <input
-          type='text'
-          className='event-number'
+          type="number"
+          className="event-number"
           value={this.state.numberOfEvents}
-          onChange={this.handleInputChanged}>
-        </input>
+          onChange={this.handleInputChanged}
+        ></input>
       </div>
-    )
+    );
   }
 }
 
