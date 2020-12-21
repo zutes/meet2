4ccf8a9b2e01b822285ac41b59ae271f5fc33d24
zutes <zutes3479@gmail.com>
2020-12-21 13:13:28 -0700

    Updated App.js
    Added ResponsiveContainer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 import { WarningAlert} from './Alert';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 
 class App extends Component {
@@ -91,9 +94,26 @@ class App extends Component {
         <h3>Enter a City</h3>
         <WarningAlert text={this.state.alertText} />
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
+        
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents}
-          updateEvents={this.updateEvents} />
+         updateEvents={this.updateEvents} />
+        <h4>Events in each city</h4>
+
+<ResponsiveContainer height={400} >
+  <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+    <CartesianGrid />
+    <XAxis type="category" dataKey="city" name="city" />
+    <YAxis
+      allowDecimals={false}
+      type="number"
+      dataKey="number"
+      name="number of events"
+    />
+    <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+    <Scatter data={this.getData()} fill="#8884d8" />
+  </ScatterChart>
+</ResponsiveContainer>
+  <EventList events={this.state.events} />
       </div>
     );
   }
